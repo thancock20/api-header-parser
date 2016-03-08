8e32edf4e26ada9d0ba0976f42de272237cd34e5
Todd Hancock <thancock20@hotmail.com>
2016-03-08 15:56:15 -0600

    Switch template engine to Jade

--- server.js
+++ server.js
@@ -5,10 +5,12 @@ var useragent = require('express-useragent');
 
 var port = process.env.PORT || 8080;
 
+app.set('view engine', 'jade');
+
 var router = express.Router();
 
 router.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
+  res.render('index', { hostname: req.hostname === 'localhost' ? 'localhost:' + port : req.hostname});
 });
 
 router.get('/main.css', function(req, res) {
